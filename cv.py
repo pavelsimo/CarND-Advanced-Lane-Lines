3c7da6c7e17cf9a1878e109699214e9dfbfec041
Pavel Simo <pavel.simo@gmail.com>
2018-09-19 14:47:09 -0700

    :memo: add TODO

--- cv.py
+++ cv.py
@@ -152,8 +152,19 @@ def unwarp(undis, src, dst):
     return warped
 
 
+def histogram(img):
+    # TO-DO: Grab only the bottom half of the image
+    # Lane lines are likely to be mostly vertical nearest to the car
+    h, w = img.shape[0], img.shape[1]
+    offset = 200
+    bottom_half = img[h // 2:h, :]
+    # TO-DO: Sum across image pixels vertically - make sure to set `axis`
+    # i.e. the highest areas of vertical lines should be larger values
+    histogram = np.sum(bottom_half, axis=0)
+    return histogram
+
+
 def plot_comparison(img1, img2, title1='', title2='', cmap1=None, cmap2=None):
-    # Plot the result
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
     f.tight_layout()
 
@@ -167,13 +178,132 @@ def plot_comparison(img1, img2, title1='', title2='', cmap1=None, cmap2=None):
     plt.show()
 
 
+def plot_histogram(y):
+    plt.plot(y)
+    plt.show()
+
+
+def find_lane_pixels(img, nwindows=9, margin=100, minpix=50):
+    """
+
+    :param img:
+    :param nwindows: number of sliding windows
+    :param margin: width of the windows +/- margin
+    :param minpix: minimum number of pixels found to recenter window
+    :return:
+    """
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(img[img.shape[0] // 2:, :], axis=0)
+    # Create an output image to draw on and visualize the result
+    out_img = np.dstack((img, img, img))
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0] // 2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Set height of windows - based on nwindows above and image shape
+    window_height = np.int(img.shape[0] // nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated later for each window in nwindows
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = img.shape[0] - (window + 1) * window_height
+        win_y_high = img.shape[0] - window * window_height
+        ### TO-DO: Find the four below boundaries of the window ###
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low),
+                      (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low),
+                      (win_xright_high, win_y_high), (0, 255, 0), 2)
+
+        ### TO-DO: Identify the nonzero pixels in x and y within the window ###
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                          (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                           (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+
+        ### TO-DO: If you found > minpix pixels, recenter next window ###
+        ### (`right` or `leftx_current`) on their mean position ###
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices (previously was a list of lists of pixels)
+    try:
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+    except ValueError:
+        # Avoids an error if the above is not implemented fully
+        pass
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    return leftx, lefty, rightx, righty, out_img
+
+
+def fit_polynomial(img, verbose=0):
+    # Find our lane pixels first
+    leftx, lefty, rightx, righty, out_img = find_lane_pixels(img)
+
+    ### TO-DO: Fit a second order polynomial to each using `np.polyfit` ###
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, img.shape[0] - 1, img.shape[0])
+    try:
+        left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+        right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+    except TypeError:
+        # Avoids an error if `left` and `right_fit` are still none or incorrect
+        print('The function failed to fit a line!')
+        left_fitx = 1 * ploty ** 2 + 1 * ploty
+        right_fitx = 1 * ploty ** 2 + 1 * ploty
+
+    # Plots the left and right polynomials on the lane lines
+    if verbose:
+        ## Visualization ##
+        # Colors in the left and right lane regions
+        out_img[lefty, leftx] = [255, 0, 0]
+        out_img[righty, rightx] = [0, 0, 255]
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+
+    return out_img
+
+
 if __name__ == '__main__':
 
     #
     # step1: undistort image
     #
-
-    img = cv2.imread('test_images/test3.jpg')
+    img = cv2.imread('test_images/test5.jpg')
     objpoints, imgpoints = load_calibration_parameters(6, 9)
     img_undistort = undistort(img, objpoints, imgpoints)
     plot_comparison(
@@ -203,7 +333,7 @@ if __name__ == '__main__':
     # step 3: gradient and threshold
     #
 
-    img_sobel = compose_threshold(img_crop, s_thresh=(170, 255), thresh=(50, 255))
+    img_sobel = compose_threshold(img_crop, s_thresh=(170, 255), thresh=(80, 255))
     plot_comparison(
         cv2.cvtColor(img_crop, cv2.COLOR_BGR2RGB),
         img_sobel,
@@ -227,3 +357,28 @@ if __name__ == '__main__':
         cmap1='gray',
         cmap2='gray'
     )
+
+    #
+    # step 5: image histogram
+    #
+    h = histogram(img_unwarp)
+    plot_histogram(h)
+
+    #
+    # step 6: fit poly
+    #
+    img_poly = fit_polynomial(img_unwarp)
+    plot_comparison(
+        img_unwarp,
+        img_poly,
+        title1='Unwrap Image',
+        title2='Poly Image',
+        cmap1='gray',
+        cmap2='gray'
+    )
+
+    #
+    # step 7: measuring curvature
+    #
+
+
